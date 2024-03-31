01ecd28699f08c3cab99fee2963aaf653e8f8d17
leon-kxng <145435067+leon-kxng@users.noreply.github.com>
2024-03-31 21:46:21 +0300

    Update index.js for resume

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -9,7 +9,6 @@ import {
   skills,
   services,
 } from "../../content_option";
-import { Link } from "react-router-dom";
 
 export const About = () => {
   return (
@@ -97,14 +96,14 @@ export const About = () => {
         </Row>
         <Row className="sec_sp">
           <Col lg={{ span: 6, offset: 3 }} className="text-center">
-            <Link to="https://raw.githubusercontent.com/leon-kxng/portfolio/master/src/assets/RÉSUMÉ.pdf">
+            <a href="https://raw.githubusercontent.com/leon-kxng/portfolio/master/src/assets/RÉSUMÉ.pdf" target="_blank">
               <button className="ac_btn btn-portfolio">
                 <span>View my RÉSUMÉ</span>
                 <div class="ring one"></div>
                 <div class="ring two"></div>
                 <div class="ring three"></div>
               </button>
-            </Link>
+            </a>
           </Col>
         </Row>
       </Container>
